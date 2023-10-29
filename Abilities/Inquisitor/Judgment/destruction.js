c66de38061f6ac1a4835345c8057eb397ec52edf
Eli Semikashev <tebesski@gmail.com>
2023-10-29 23:57:09 +0200

    added Inquisitor Judgments

---
+++ Abilities/Inquisitor/Judgment/destruction.js
@@ -0,0 +1,9 @@
+const targetMacro = "applyBuff"
+const params = "apply Destruction to me at @attributes.hd.total"
+
+window.macroChain = window.macroChain?.concat([params]) ?? [params]
+game.macros.find((o) => o.name == targetMacro)?.execute()
+
+const judgement = actor.items
+   .get(actor.system.resources.classFeat_judgment._id)
+   .addCharges(-1)
---
+++ Abilities/Inquisitor/Judgment/protection.js
@@ -0,0 +1,8 @@
+const targetMacro = "applyBuff"
+const params = "toggle Protection to me at @attributes.hd.total"
+window.macroChain = window.macroChain?.concat([params]) ?? [params]
+game.macros.find((o) => o.name == targetMacro)?.execute()
+
+const judgement = actor.items
+   .get(actor.system.resources.classFeat_judgment._id)
+   .addCharges(-1)
