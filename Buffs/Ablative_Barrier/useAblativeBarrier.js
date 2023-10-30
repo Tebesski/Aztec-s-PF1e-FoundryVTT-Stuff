69f852c9dbf7bfeb11e6d2a20ad6935c0b7f2b15
Eli Semikashev <tebesski@gmail.com>
2023-10-30 17:13:45 +0200

    minor updates

--- Buffs/Ablative_Barrier/useAblativeBarrier.js
+++ Buffs/Ablative_Barrier/useAblativeBarrier.js
@@ -7,19 +7,13 @@ const barrier = await actor.items.getName("Ablative Barrier").system.uses.value
 
 await Dialog.prompt({
    title: "Amount of damage:",
-   content: "<input id='dmgInput' type='number' value='0' />",
+   content: `<input id='dmgInput' type='number' value='0' />`,
    label: "Apply",
    callback: (html, event) => main(html, event),
 })
 
 async function main(html) {
    const dmg = Number(html.find("input#dmgInput").val())
-
-   if (barrier < 1) {
-      window.macroChain = window.macroChain?.concat([params]) ?? [params]
-      game.macros.find((o) => o.name == targetMacro)?.execute()
-      return
-   }
 
    if (hp.value + 5 >= hp.max) {
       await actor.update({
@@ -34,18 +28,50 @@ async function main(html) {
          .get(actor.system.resources.ablativeBarrier._id)
          .addCharges(-(hp.max - hp.value))
 
+      await Dialog.confirm({
+         title: "Total damage taken",
+         content: `${Math.floor(dmg - 5)}`,
+         buttons: {
+            okay: {
+               label: "Okay",
+               icon: `<i class="fas fa-check"></i>`,
+            },
+         },
+         default: "okay",
+      })
+
       return
    }
 
    await actor.update({
       "system.attributes.hp.value": hp.value + Math.min(dmg, 5),
    })
+
    await actor.update({
       "system.attributes.hp.nonlethal": hp.nonlethal + Math.min(dmg, 5),
    })
+
    actor.items
       .get(actor.system.resources.ablativeBarrier._id)
       .addCharges(-Math.min(dmg, 5))
+
+   await Dialog.confirm({
+      title: "Total damage taken",
+      content: `${Math.floor(dmg - 5)}`,
+      buttons: {
+         okay: {
+            label: "Okay",
+            icon: `<i class="fas fa-check"></i>`,
+         },
+      },
+      default: "okay",
+   })
+
+   if (barrier < 1) {
+      window.macroChain = window.macroChain?.concat([params]) ?? [params]
+      game.macros.find((o) => o.name == targetMacro)?.execute()
+      return
+   }
 }
 
 main()
